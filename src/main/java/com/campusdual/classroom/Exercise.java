a1f1b2b264aff4502b79c46294a3452d0c26f6ac
Mario Rodríguez Martínez <mario.rodriguez.martinez.lu@gmail.com>
2025-11-19 09:48:25 +0100

    Solved exercise 27

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -1,7 +1,9 @@
 package com.campusdual.classroom;
 
+
+
 public class Exercise {
     public static void main(String[] args) {
-
+        throw new RuntimeException("Esto es una excepción");
     }
 }
